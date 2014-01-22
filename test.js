016fc85aed5cb8deca98beeff3ef74012998e399
Marshall Rose <mrose17@gmail.com>
2014-01-21 16:15:15 -0800

    smaller test

--- test.js
+++ test.js
@@ -3,7 +3,7 @@ var ClientAPI = require('./taas-client')
   ;
 
 var steward = new ClientAPI.ClientAPI(
-{ steward : { name    : 'arden-arcade'
+{ steward : { name    : 'zephyr'
             , crtPath : 'server.crt'
             }
 , cloud   : { service : 'taas.thethingsystem.net'
@@ -14,7 +14,7 @@ var steward = new ClientAPI.ClientAPI(
 
   if (!loginP) return;
 
-  steward.login('mrose/7', '429305', function(err, error) {
+  steward.login('root/1', '000000', function(err, error) {
     if (!!err) {
       console.log('login error: ' + JSON.stringify(error));
       process.exit(0);
@@ -26,12 +26,6 @@ var steward = new ClientAPI.ClientAPI(
   console.log('ready ' + channel + ' data='+ JSON.stringify(data));
 
   if (channel === 'management') console.log('ready, set, go!');
-}).on('actor', function(whoami, whatami) {
-  if (whoami === 'place/1') return console.log('actor ' + whoami);
-
-  if (whatami !== '/device/motive/crazyflie/3d') return;
-
-  console.log(util.inspect(steward.actors[whoami], { depth: null }));
 }).on('close', function(channel) {
   console.log(channel + ' close');
   process.exit(0);
@@ -39,3 +33,4 @@ var steward = new ClientAPI.ClientAPI(
   console.log(channel + ' error: ' + err.message);
   process.exit(0);
 });
+};
